29b9774ce2ab65e824513af4911b2d40321a83e5
Daffa Firtiyan <mdaffa.anzalna@gmail.com>
2023-01-13 20:17:50 +1300

    youtube mp4 -> mp3 (working)
    need to remove mp4 file after it's done converting

--- youtubeDownloader/main.py
+++ youtubeDownloader/main.py
@@ -1,12 +1,7 @@
 from pytube import YouTube
-from sys import argv
 
-# first command line when we run the program
-link = argv[1]
+link = input("link here: ")
 yt = YouTube(link)
-
-print("Title: ", yt.title)
-print("View: ", yt.views)
 
 yd = yt.streams.get_highest_resolution()
 yd.download('/Users/mdaff/Documents/Downloaded Videos')
---
+++ youtubeDownloader/yt2mp3.py
@@ -0,0 +1,35 @@
+import os
+import csv
+from pytube import YouTube
+from moviepy.editor import *
+
+outFolder = "F:\mp3_files"
+if not os.path.exists(outFolder):
+    os.mkdir(outFolder)
+
+# specify the csv file containing the youtube video urls
+csv_file = "youtubeDownloader\youtube_videos.csv"
+
+# open the csv file and read the video urls
+with open(csv_file, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        url = row[0]
+        yt = YouTube(url)
+        print(yt.title)
+        vid = yt.streams.filter(file_extension='mp4',res='720p').first()
+
+        print("Downloading video...")
+        vid.download(output_path=outFolder)
+
+        input = os.path.join(outFolder, vid.default_filename)
+        output = os.path.join(outFolder, vid.default_filename.replace(".mp4", ".mp3"))
+
+        vid = VideoFileClip(input)
+
+        print("Extracting audio...")
+        audio = vid.audio
+
+        audio.write_audiofile(output)
+
+        # os.remove(input)
